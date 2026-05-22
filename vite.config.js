cceb18367d323e4f9c223d0d0e6a05ec790a7401
Eldar Semenov <semenoveldar09@gmail.com>
2026-05-23 02:07:12 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://moviediary-backend-production.up.railway.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
